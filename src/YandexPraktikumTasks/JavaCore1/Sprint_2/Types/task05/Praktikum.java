9d6977ba4f3a45a82437500a1c52fda28d890141
living221 <living221@gmail.com>
2022-09-12 18:37:47 +0700

    Yandex Praktikum Core 1 Tasks (Types).

---
+++ src/YandexPraktikumTasks/JavaCore1/Sprint_2/Types/task05/Praktikum.java
@@ -0,0 +1,18 @@
+package YandexPraktikumTasks.JavaCore1.Sprint_2.Types.task05;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 3;
+        int c = -7;
+        Integer[] numbers = new Integer[3];
+
+        // Сохраните в массив переменные a, b и с
+        numbers[0] = a;
+        numbers[1] = b;
+        numbers[2] = c;
+        // Напечатайте все элементы
+        System.out.println("Сейчас в массиве числа: " + numbers[0] + ","
+                + numbers[1] + "," + numbers[2]);
+    }
+}
---
+++ src/YandexPraktikumTasks/JavaCore1/Sprint_2/Types/task09/Bus.java
@@ -0,0 +1,9 @@
+package YandexPraktikumTasks.JavaCore1.Sprint_2.Types.task09;
+
+class Bus {
+    public Bus(int initialNumber) {
+        ticketNumber = initialNumber;
+    }
+
+    int ticketNumber;
+}
